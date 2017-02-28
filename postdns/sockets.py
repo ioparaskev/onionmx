5d5b547ff20b52581140f7ca04185bc4190e9526
John Paraskevopoulos <io.paraskev@gmail.com>
2017-02-28 17:34:19 +0200

    Change socket reuse time
    Avoid cases when client closes after server shutdown that will result in
    bind "freeze" for 1 min due to TCP time_wait
    more info: https://hea-www.harvard.edu/~fine/Tech/addrinuse.html

--- postdns/sockets.py
+++ postdns/sockets.py
@@ -32,6 +32,8 @@ def resolve(rerouter, conn, resolve_callback=lambda q, a: (q, a)):
 
 def daemonize_server(rerouter, host, port, resolver=resolve):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # add reuse to 1 sec to avoid reuse error when server closes before client
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((host, port))
     sock.listen(1)
     atexit.register(close_socket, sock=sock)
